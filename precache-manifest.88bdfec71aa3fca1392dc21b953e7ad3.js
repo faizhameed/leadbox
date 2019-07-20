426f167724bb6669973351194be7f023d35b8475
Faiz Hameed <faizftb@gmail.com>
2019-07-20 11:26:48 +0530

    Updates

---
+++ precache-manifest.88bdfec71aa3fca1392dc21b953e7ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e97d5a050493723a45b537d3c2e2ff7",
+    "url": "/leadbox/index.html"
+  },
+  {
+    "revision": "3dab653d44755bd3fc1c",
+    "url": "/leadbox/static/css/main.3f2775fa.chunk.css"
+  },
+  {
+    "revision": "871b111d62b16ec70d18",
+    "url": "/leadbox/static/js/2.a75396da.chunk.js"
+  },
+  {
+    "revision": "3dab653d44755bd3fc1c",
+    "url": "/leadbox/static/js/main.5420c2f7.chunk.js"
+  },
+  {
+    "revision": "d23408c3ae2c85643b1e",
+    "url": "/leadbox/static/js/runtime~main.b4ea0879.js"
+  },
+  {
+    "revision": "d7189b57d1024bf97e8ae57d9ca0cb5c",
+    "url": "/leadbox/static/media/Mask Group 3.d7189b57.svg"
+  },
+  {
+    "revision": "19bd94aef764a4379d5e4cb63874cf88",
+    "url": "/leadbox/static/media/noun_Download_2120379.19bd94ae.svg"
+  }
+]);
